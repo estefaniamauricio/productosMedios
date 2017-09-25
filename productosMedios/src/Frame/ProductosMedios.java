4c07a9c46b2fb1d15027684cac709fa9e9338c87
IVAN <IVAN@DANIEL>
2017-09-25 09:10:59 -0500

    Color

--- productosMedios/src/Frame/ProductosMedios.java
+++ productosMedios/src/Frame/ProductosMedios.java
@@ -2,7 +2,6 @@
  */
 package Frame;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
@@ -16,25 +15,27 @@ import javax.swing.JTextField;
 
 public class ProductosMedios extends JFrame {
 
-    private JLabel x0, x1, ri;
-    private JTextField textoX0, textoX1, textoRi;
+    private JLabel x0, x1, ri, D;
+    private JTextField textoX0, textoX1, textoRi, textoD;
     private JButton calcular, borrar, guardar;
     private JTable tabla;
 
     public ProductosMedios() {
         setLayout(null);
-        setBackground(Color.BLUE);
-        
-        
+        getContentPane().setBackground(new Color(100, 100, 250));
+
         x0 = new JLabel();
         textoX0 = new JTextField();
-        
+
         x1 = new JLabel();
         textoX1 = new JTextField();
-        
+
         ri = new JLabel();
         textoRi = new JTextField();
         
+        D = new JLabel();
+        textoD = new JTextField();
+
         calcular = new JButton("Calcular");
         borrar = new JButton("Borrar");
         guardar = new JButton("Guardar");
@@ -45,45 +46,60 @@ public class ProductosMedios extends JFrame {
 
     private void construir() {
         Manejador objManejador = new Manejador();
-        
+
         x0.setText("x0=");
         x0.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+        x0.setForeground(Color.YELLOW);
         x0.setBounds(45, 40, 50, 50);
-        
-        textoX0.setEnabled(true);
+
         textoX0.setFont(new Font("Cambria Math", Font.PLAIN, 18));
         textoX0.setToolTipText("Semilla x0");
         textoX0.setBounds(80, 53, 100, 25);
-        
+
         x1.setText("x1=");
         x1.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+        x1.setForeground(Color.YELLOW);
         x1.setBounds(193, 40, 50, 50);
-        
-        textoX1.setEnabled(true);
+
         textoX1.setFont(new Font("Cambria Math", Font.PLAIN, 18));
         textoX1.setToolTipText("Semilla x1");
         textoX1.setBounds(230, 53, 100, 25);
-        
+
         ri.setText("ri=");
         ri.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+        ri.setForeground(Color.YELLOW);
         ri.setBounds(350, 40, 50, 50);
-                
-        textoRi.setEnabled(true);
+
         textoRi.setFont(new Font("Cambria Math", Font.PLAIN, 18));
         textoRi.setToolTipText("Número de iteraciones");
         textoRi.setBounds(380, 53, 50, 25);
         
-        calcular.setBounds(15, 0, 100, 40);
+        D.setText("D=");
+        D.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+        D.setForeground(Color.YELLOW);
+        D.setBounds(175, 80, 50, 50);
+                
+        textoD.setFont(new Font("Cambria Math", Font.PLAIN, 18));
+        textoD.setToolTipText("Número de dígitos");
+        textoD.setBounds(205, 93, 100, 25);
+
+        calcular.setBounds(0, 0, 150, 40);
+        calcular.setBackground(new Color(100, 255, 100));
+        calcular.setForeground(Color.BLACK);
         calcular.setCursor(new Cursor(Cursor.HAND_CURSOR));
         calcular.setFont(new Font("Verdana", Font.BOLD, 12));
         calcular.addActionListener(objManejador);
-        
-        borrar.setBounds(130, 0, 100, 40);
+
+        borrar.setBounds(175, 0, 150, 40);
+        borrar.setBackground(new Color(100, 255, 100));
+        borrar.setForeground(Color.BLACK);
         borrar.setCursor(new Cursor(Cursor.HAND_CURSOR));
         borrar.setFont(new Font("Verdana", Font.BOLD, 12));
         borrar.addActionListener(objManejador);
-          
-        guardar.setBounds(200, 0, 100, 40);
+
+        guardar.setBounds(350, 0, 150, 40);
+        guardar.setBackground(new Color(100, 255, 100));
+        guardar.setForeground(Color.BLACK);
         guardar.setCursor(new Cursor(Cursor.HAND_CURSOR));
         guardar.setFont(new Font("Verdana", Font.BOLD, 12));
         guardar.addActionListener(objManejador);
@@ -95,32 +111,35 @@ public class ProductosMedios extends JFrame {
         add(x1);
         add(textoX1);
         add(ri);
+        add(D);
+        add(textoD);
         add(textoRi);
         add(calcular);
         add(borrar);
         add(guardar);
     }
-    
-    private class Manejador implements ActionListener{
+
+    private class Manejador implements ActionListener {
+
         @Override
-        public void actionPerformed(ActionEvent ae){
+        public void actionPerformed(ActionEvent ae) {
             if (ae.getSource() == calcular) {
                 System.out.println("calcular");
             }
-            
+
             if (ae.getSource() == borrar) {
                 System.out.println("borrar");
             }
-            
+
             if (ae.getSource() == guardar) {
                 System.out.println("guardar");
             }
         }
     }
-    
+
     public static void main(String[] args) {
         ProductosMedios pm = new ProductosMedios();
-        
+
         pm.setSize(500, 600);
         pm.setDefaultCloseOperation(EXIT_ON_CLOSE);
         pm.setResizable(false);
---
+++ productosMedios/src/Frame/TablaEjemplo.java
@@ -0,0 +1,54 @@
+package Frame;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TablaEjemplo extends JFrame {
+
+    public TablaEjemplo() {
+        Object[][] datos = {
+            {"Juan", new Integer(25), new Boolean(false)},
+            {"Sonia", new Integer(33), new Boolean(true)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+            {"Pedro", new Integer(42), new Boolean(false)},
+        };
+
+        String[] columnNames = {"Nombre", "Años", "Apto",};
+        DefaultTableModel dtm = new DefaultTableModel(datos, columnNames);
+        final JTable table = new JTable(dtm);
+
+        // Agregar nueva columna
+        String[] columnaNueva1 = {"vago", "diestro", "normal",};
+        dtm.addColumn("Tipo", columnaNueva1);
+
+        // Agregar nueva fila
+        Object[] newRow = {"Maria", new Integer(55), new Boolean(false)};
+        dtm.addRow(newRow);
+
+        // Modificar celda especifica
+        dtm.setValueAt("XXX", 3, 3); // Row/Col
+        table.setPreferredScrollableViewportSize(new Dimension(250, 100));
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        TablaEjemplo frame = new TablaEjemplo();
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
